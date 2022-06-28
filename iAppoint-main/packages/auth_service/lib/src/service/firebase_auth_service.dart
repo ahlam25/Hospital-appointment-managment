fd69462b67efbdf05a5f1a7d11ed928a55af6a5d
Kaha <kahahai@gmail.com>
2022-06-28 17:52:39 +0800

    I-appoint
    Mobile app final assessment

---
+++ iAppoint-main/packages/auth_service/lib/src/service/firebase_auth_service.dart
@@ -0,0 +1,98 @@
+import 'package:auth_service/src/models/models.dart';
+import 'package:auth_service/src/service/auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseAuthService implements AuthService {
+  FirebaseAuthService({
+    required FirebaseAuth authService,
+  }) : _firebaseAuth = authService;
+
+  final FirebaseAuth _firebaseAuth;
+
+  UserEntity _mapFirebaseUser(auth.User? user) {
+    if (user == null) {
+      return UserEntity.empty();
+    }
+
+    var splittedName = ['Name ', 'LastName'];
+    if (user.displayName != null) {
+      splittedName = user.displayName!.split(' ');
+      print(user);
+    }
+
+    final map = <String, dynamic>{
+      'id': user.uid,
+      'firstName': splittedName.first,
+      'lastName': splittedName.last,
+      'email': user.email ?? '',
+      'emailVerified': user.emailVerified,
+      'imageUrl': user.photoURL ?? '',
+      'isAnonymous': user.isAnonymous,
+      'age': 0,
+      'phoneNumber': '',
+      'address': '',
+    };
+    return UserEntity.fromJson(map);
+  }
+
+  @override
+  Future<UserEntity> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final userCredential =
+          await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      return _mapFirebaseUser(userCredential.user!);
+    } on auth.FirebaseAuthException catch (e) {
+      throw _determineError(e);
+    }
+  }
+
+  @override
+  Future<UserEntity> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: "abubakaryusuf951@gmail.com",
+        password: "password",
+      );
+
+      return _mapFirebaseUser(_firebaseAuth.currentUser!);
+    } on FirebaseAuthException catch (e) {
+      throw _determineError(e);
+    }
+  }
+
+  AuthError _determineError(FirebaseAuthException exception) {
+    switch (exception.code) {
+      case 'invalid-email':
+        return AuthError.invalidEmail;
+      case 'user-disabled':
+        return AuthError.userDisabled;
+      case 'user-not-found':
+        return AuthError.userNotFound;
+      case 'wrong-password':
+        return AuthError.wrongPassword;
+      case 'email-already-in-use':
+      case 'account-exists-with-different-credential':
+        return AuthError.emailAlreadyInUse;
+      case 'invalid-credential':
+        return AuthError.invalidCredential;
+      case 'operation-not-allowed':
+        return AuthError.operationNotAllowed;
+      case 'weak-password':
+        return AuthError.weakPassword;
+      case 'ERROR_MISSING_GOOGLE_AUTH_TOKEN':
+      default:
+        return AuthError.error;
+    }
+  }
+}
